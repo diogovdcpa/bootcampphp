12e6a85e1477d7e6e6c209d40cef40e732ba50b3
Diogo Caldas <diogovdcpa@gmail.com>
2020-04-03 20:17:37 -0300

    criando array e condiçoes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bootcampphp
-Projetos do Bootcamp PHP 
+Projeto do Bootcamp PHP 
+
+Desafio : 
+Criar um projeto em php para uma competição de natação, onde o sistema deverá
+permitir a inserção de competidores com seu nome e idade, e o sistema deverá
+identificar em qual categoria o nadador irá competir, por exemplo, 6 a 12 anos
+categoria inftantil, de 13 a 18 categoria adolescente, acima de 18 categoria adulto,
+o sistema deverá retornar a categoria para cada nadador que for cadastrado.
--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php
 
+// variavel do tipo array 
+$categorias = [];
 
+// dados do array
+$categorias[] = 'Infantil';
+$categorias[] = 'Adolescente';
+$categorias[] = 'Adulto';
+$categorias[] = 'Idoso';
 
-?>
+// print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = 12;
+
+// var_dump($nome);  
+// var_dump($idade);
+
+if($idade >=6 && $idade <=12){
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'Infantil')
+        {
+            echo "O nadador ".$nome." compete na categoria ".$categorias[$i];
+        }
+    }
+}else if($idade >=13 && $idade<=17){
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'Adolescente')
+        {
+            echo "O nadador ".$nome." compete na categoria ".$categorias[$i];
+        }
+    }
+}else{
+    for($i = 0; $i < count($categorias); $i++)
+    {
+        if($categorias[$i] == 'Adulto')
+        {
+            echo "O nadador ".$nome." compete na categoria ".$categorias[$i];
+        }
+    }
+}
+
